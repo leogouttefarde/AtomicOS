9837c0a1fd7047daad655871341e38b66db88795
Etienne Quay-Thevenon <etienne.quay-thevenon@ensimag.grenoble-inp.fr>
2016-05-19 16:59:03 +0200

    Début du travail sur le shell
    Console ok
    Pour l'instant le shell est dan sle kernel (à changer)

---
+++ kernel/shell.c
@@ -0,0 +1,24 @@
+#include "clavier.h"
+#include <stdbool.h>
+#include <string.h>
+
+static void interpreter (const char *ligne) {
+	if (strcmp("echo",ligne) == 0) {
+		cons_echo(0);
+	}
+	else {
+		cons_write("commande introuvable\n",21);
+	}
+}
+
+int shell (void *p) {
+	if (p==NULL) {}
+	while(true) {
+		cons_write(">",1);
+		char test [10];
+		unsigned long size = cons_read(test,9);
+		test[size+1]='\0';
+		interpreter(test);
+		//cons_write("\n",1);
+	}
+}
