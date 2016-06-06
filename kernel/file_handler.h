752e5993c62e40a9db66c0c0fea59522db9ce081
Azizi OMAR <Azizi.Omar@ensimag.grenoble-inp.fr>
2016-06-06 15:46:49 +0200

    Sujet: Début de Système de fichier
    1) C'est de notre propre système implémenté dans la RAM, donc ce n'est pas persistant
    2) A continuer, fonctions à implémenter : putBinaryData & modifyBinaryData

---
+++ kernel/file_handler.h
@@ -0,0 +1,19 @@
+
+#ifndef __FILE_HANDLER_H__
+#define __FILE_HANDLER_H__
+
+#include <stdint.h>
+
+//La création d'un fichier
+int createFile(const char *filename, uint32_t filesize);
+
+//La suppression d'un fichier
+int deleteFile (const char *filename);
+
+//Le renommage d'un fichier 
+int renameFile(const char *filename1, const char *filename2);
+
+//L'affichage de nom de tous les fichiers
+void listAllFiles();
+
+#endif
