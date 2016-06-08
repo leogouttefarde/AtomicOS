5e39b3ddfc1b652f3f41f99a60283457515c302a
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-06-08 02:07:25 +0200

    Images: Amélioration du passage en mode visionnage
    Amélioration du passage en mode visionnage lors de l'affichage d'images.

--- kernel/process.c
+++ kernel/process.c
@@ -1222,8 +1222,8 @@ int syscall(int num, int arg0, int arg1, int arg2, int arg3, int arg4)
 		break;
 
 	case DISPLAY:
-		ret = init_graphics(1366, 768, 32);
-		// ret = init_graphics(1680, 1050, 32);
+		init_vbe_mode(findMode(1366, 768, 32));
+		// init_vbe_mode(findMode(1680, 1050, 32));
 		ret = display((char*)arg0);
 		break;
 
